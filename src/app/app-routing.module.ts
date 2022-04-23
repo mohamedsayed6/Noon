9e989dea264861dffad9913e25a557e4693fd396
Mohamed SayedAhmed Hassani <msayeda70@gmail.com>
2022-04-23 16:01:23 +0200

    Mohamed-NotFoundPage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { NotFoundComponent } from './Shared/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",redirectTo:'/Noon',pathMatch:'full'},
+  {path:"Noon",component:HomeComponent},
+  {path:"**",component:NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
