fe006bb9ff6144e2ae0c8438b58860fde3995c69
Mohamed SayedAhmed Hassani <msayeda70@gmail.com>
2022-04-23 14:54:59 +0200

    Init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Components/home/home.component';
+import { UserComponent } from './Components/user/user.component';
+import { CartComponent } from './Components/cart/cart.component';
+import { HomeHeaderComponent } from './Shared/home-header/home-header.component';
+import { HomeFooterComponent } from './Shared/home-footer/home-footer.component';
+import { UserHeaderComponent } from './Shared/user-header/user-header.component';
+import { UserFooterComponent } from './Shared/user-footer/user-footer.component';
+import { SignInComponent } from './Shared/sign-in/sign-in.component';
+import { SignUpComponent } from './Shared/sign-up/sign-up.component';
+import { NotFoundComponent } from './Shared/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UserComponent,
+    CartComponent,
+    HomeHeaderComponent,
+    HomeFooterComponent,
+    UserHeaderComponent,
+    UserFooterComponent,
+    SignInComponent,
+    SignUpComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
