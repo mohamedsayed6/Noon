98a924a1fcca78d5ca60282665a9028f4bc5304b
Hady Wafa <97hadywafa@gmail.com>
2022-05-01 01:53:15 +0200

    comment error of localization

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,31 +1,33 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { AllProductsComponent } from './Components/home/all-products/all-products.component';
-import { CategoryProductsComponent } from './Components/home/category-products/category-products.component';
-import { HomeComponent } from './Components/home/home.component';
+import { ProductDetailsComponent } from "./Components/product-details/product-details.component";
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { AllProductsComponent } from "./Components/home/all-products/all-products.component";
+import { CategoryProductsComponent } from "./Components/home/category-products/category-products.component";
+import { HomeComponent } from "./Components/home/home.component";
 
-import { NotFoundComponent } from './Shared/not-found/not-found.component';
-import { PayPalComponent } from './Shared/pay-pal/pay-pal.component';
-import { SignInComponent } from './Shared/sign-in/sign-in.component';
+import { NotFoundComponent } from "./Shared/not-found/not-found.component";
+import { PayPalComponent } from "./Shared/pay-pal/pay-pal.component";
+import { SignInComponent } from "./Shared/sign-in/sign-in.component";
 
 const routes: Routes = [
-  {path:"",redirectTo:'/Noon',pathMatch:'full'},
-  {path:"Noon",component:HomeComponent},
-  {path:"SignIn",component:SignInComponent},
-  {path:"All Products",component:AllProductsComponent},
-  {path:"Category/:cid",component:CategoryProductsComponent},
-  { 
-    path: 'user', 
-    loadChildren: () => import(`./user/user-routing/user-routing.module`).then(
-      module => module.UserRoutingModule
-    )
-  },
-  {path:"PayPal",component:PayPalComponent},
-  {path:"**",component:NotFoundComponent}
+  { path: "", redirectTo: "/Noon", pathMatch: "full" },
+  { path: "Noon", component: HomeComponent },
+  { path: "SignIn", component: SignInComponent },
+  { path: "All Products", component: AllProductsComponent },
+  { path: "Category/:cid", component: CategoryProductsComponent },
+  { path: "Product/:id", component: ProductDetailsComponent },
+  // {
+  //   path: 'user',
+  //   loadChildren: () => import(`./user/user-routing/user-routing.module`).then(
+  //     module => module.UserRoutingModule
+  //   )
+  // },
+  { path: "PayPal", component: PayPalComponent },
+  { path: "**", component: NotFoundComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,39 +1,37 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { UsersideComponent } from './userside/userside.component';
-import { OrderComponent } from './order/order.component';
-import { AddressComponent } from './address/address.component';
-import { ProfileComponent } from './profile/profile.component';
-import { RouterModule } from '@angular/router';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {TranslateHttpLoader} from '@ngx-translate/http-loader';
-import {TranslateModule} from '@ngx-translate/core';
-import { HttpClient } from '@angular/common/http';
+// import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// import { UsersideComponent } from './userside/userside.component';
+// import { OrderComponent } from './order/order.component';
+// import { AddressComponent } from './address/address.component';
+// import { ProfileComponent } from './profile/profile.component';
+// import { RouterModule } from '@angular/router';
+// import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+// import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+// import {TranslateModule} from '@ngx-translate/core';
+// import { HttpClient } from '@angular/common/http';
 
-export function userHttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http,'./assets/i18n/','.json');
-}
+// export function userHttpLoaderFactory(http: HttpClient) {
+//   return new TranslateHttpLoader(http,'./assets/i18n/','.json');
+// }
 
-@NgModule({
-  declarations: [
-    
- 
-  
-  ],
-  imports: [
-    CommonModule,
-    RouterModule,
-    FormsModule,
-    ReactiveFormsModule,
-    TranslateModule.forChild({
-      loader: {
-      provide: TranslateModule,
-      useFactory: userHttpLoaderFactory,
-      deps: [HttpClient]
-  },
-  defaultLanguage:"ar"} )
+// @NgModule({
+//   declarations: [
 
-  ],
-  exports:[RouterModule]
-})
-export class UserModule { }
+//   ],
+//   imports: [
+//     CommonModule,
+//     RouterModule,
+//     FormsModule,
+//     ReactiveFormsModule,
+//     TranslateModule.forChild({
+//       loader: {
+//       provide: TranslateModule,
+//       useFactory: userHttpLoaderFactory,
+//       deps: [HttpClient]
+//   },
+//   defaultLanguage:"ar"} )
+
+//   ],
+//   exports:[RouterModule]
+// })
+// export class UserModule { }
